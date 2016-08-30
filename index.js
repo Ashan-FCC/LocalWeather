1e97891320a113979e2e8d36e7050b35fa569ed1
Ashan <ashan@ibaht.com>
2016-08-30 19:31:51 +0700

    Show location of user. Change background color and font color

--- index.js
+++ index.js
@@ -21,17 +21,48 @@ $(document).ready(function() {
 
 function showPosition(position) {
   userLocation = position;
+  var longitude = position.coords.longitude;
+  var latitude = position.coords.latitude;
   console.log("showPosition: " + userLocation);
-  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " +
-    position.coords.longitude;
-  getWeather(position.coords.longitude, position.coords.latitude);
+  x.innerHTML = "Latitude: " + longitude + "<br>Longitude: " +
+    latitude;
+  setUserLocation(longitude,latitude);
+  getWeather(longitude, latitude);
   return
   position;
 }
 
+function setUserLocation(longitude,latitude){
+    $.ajax({
+    url: "http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude,
+    method: "GET",
+    success: function(response) {
+      currentWeather = response;
+      var jsonString = JSON.stringify(response);
+      var result = JSON.stringify(response);
+      console.log(result);
+      var results = response["results"];
+      console.log("Results: "+results);
+      results.forEach(getSublocality);
+      console.log("Request Success!");
+      //console.log(response);
+      console.log("In the success func of ajax");
+    }
+  });
+}
+function getSublocality(val, ind,arr){
+  var types = val["types"];
+  for(var i = 0; i< types.length; i++){
+    if(types[i]==="sublocality"){
+      $("#location").html(val["formatted_address"]);
+      return;
+    }
+  }
+}
+
 function getWeather(longitude, latitude) {
-  longitude = longitude.toFixed(2);
-  latitude = latitude.toFixed(2);
+  longitude = longitude.toFixed(6);
+  latitude = latitude.toFixed(6);
   console.log("long: " + longitude);
   console.log("lat: " + latitude);
   $.ajax({
@@ -58,5 +89,22 @@ function showWeather(response){
 }
 
 
+
+function convertToCelcisu(farenheit){
+  var celcius = (farenheit-32)/1.8;
+  return celcius;
+}
+
+function convertToFarenheit(celcius){
+  var farenheit = (celcius*1.8) + 32;
+}
 });
 
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
