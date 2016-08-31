ff0d9e856c05372789f0b9d1dcabb243b6f525e2
Ashan <ashan@ibaht.com>
2016-08-31 18:00:22 +0700

    Added google maps api to get accurate location.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var x = document.getElementById("demo");
 var currentWeather = "";
 var userLocation = "";
-
+var defaultWeather = {
+                      "weather" : [{
+                        "icon" : "10d", 
+                        "main" : "",
+                        "description" : "Light Rain"
+                        }],
+                      "main" : {
+                        "temp" : "300.43",
+                        "humidity" : "55"
+                      },
+                      "wind":{
+                     "speed":5.1,
+                     "deg":210
+                      }
+                    };
 
 $(document).ready(function() {
+
   getLocation(getWeather);
 
   function getLocation() {
@@ -14,22 +29,15 @@ $(document).ready(function() {
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
-  console.log("getLocation: " + userLocation);
-  console.log("Finishing getLocation");
   return userLocation;
 }
 
 function showPosition(position) {
-  userLocation = position;
   var longitude = position.coords.longitude;
   var latitude = position.coords.latitude;
-  console.log("showPosition: " + userLocation);
-  x.innerHTML = "Latitude: " + longitude + "<br>Longitude: " +
-    latitude;
   setUserLocation(longitude,latitude);
   getWeather(longitude, latitude);
-  return
-  position;
+  return  position;
 }
 
 function setUserLocation(longitude,latitude){
@@ -40,20 +48,22 @@ function setUserLocation(longitude,latitude){
       currentWeather = response;
       var jsonString = JSON.stringify(response);
       var result = JSON.stringify(response);
-      console.log(result);
-      var results = response["results"];
-      console.log("Results: "+results);
-      results.forEach(getSublocality);
-      console.log("Request Success!");
-      //console.log(response);
-      console.log("In the success func of ajax");
+      if(response["status"]==="OK"){
+        var results = response["results"];
+        results.forEach(getLocality);
+      }else{
+         $("#location").text("Couldn't get Address");
+      }
+    },
+    error: function(response) {
+        $("#location").text("Couldn't get Address");
     }
   });
 }
-function getSublocality(val, ind,arr){
+function getLocality(val, ind,arr){
   var types = val["types"];
   for(var i = 0; i< types.length; i++){
-    if(types[i]==="sublocality"){
+    if(types[i]==="locality"){
       $("#location").html(val["formatted_address"]);
       return;
     }
@@ -63,48 +73,74 @@ function getSublocality(val, ind,arr){
 function getWeather(longitude, latitude) {
   longitude = longitude.toFixed(6);
   latitude = latitude.toFixed(6);
-  console.log("long: " + longitude);
-  console.log("lat: " + latitude);
   $.ajax({
     url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=28e6874e972fc7cb18a81eb5ad0ede75",
     method: "GET",
     success: function(response) {
-      currentWeather = response;
-      var jsonString = JSON.stringify(response);
-      console.log("Request Success!");
-      console.log(response);
+      console.log("Success");
       showWeather(response);
-      console.log("In the success func of ajax");
+    },
+    error: function(response) {
+      console.log("Error");
+      showWeather(defaultWeather);
+
     }
   }); //
 }
 
 function showWeather(response){
+  console.log(JSON.stringify(response));
 	var weather = response.weather[0];
-	var weatherStats = response.main;
-	$(".description").html(weather.main);
-	$(".temperature").html(weatherStats.temp);
-	$(".humidity").html(weatherStats.humidity);
-	
+	var weatherStats = toCamelCase(weather.description);
+  var urlid = weather.icon;
+  var iconUrl = "http://openweathermap.org/img/w/"+urlid+".png";
+  var icon = "<img src="+iconUrl+ " alt="+weather.main+" width=50px height=50px/>";
+  var wind = response.wind.speed;
+	$(".description").text(weatherStats);
+  var temp = fromKelvinToCelcius(response.main.temp)
+	$(".temperature-value").text(temp);
+	$(".humidity > span").text(response.main.humidity+" %");
+  $(".weather-icon").html(icon);
+  $(".wind > span").text(wind+" mph");
+
 }
 
 
+function fromKelvinToCelcius(temp) {
+  return (temp-273).toFixed(2);
+}
 
-function convertToCelcisu(farenheit){
+function convertToCelcius(farenheit){
   var celcius = (farenheit-32)/1.8;
   return celcius;
 }
 
 function convertToFarenheit(celcius){
   var farenheit = (celcius*1.8) + 32;
+  return farenheit;
 }
+
+function toCamelCase(str){
+  console.log("Converting to camel case: "+str );
+  return str.split(' ').map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ');
+}
+
+$(".temp-unit").on("click", function(){
+    var temp = $(".temperature-value").text();
+    var unit = $(".temp-unit").text();
+    console.log("Value of temp-unit: "+unit);
+    console.log("Value of temp: "+temp);
+    if(unit==="° C"){
+      $(".temp-unit").html("° F");
+      temp = convertToFarenheit(temp);
+    }else{
+       $(".temp-unit").html("° C");
+       temp  = convertToCelcius(temp);
+    }
+    $(".temperature-value").html(temp.toFixed(2));
+})
+
 });
 
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
